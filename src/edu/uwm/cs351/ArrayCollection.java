1ede919d3564c83fe7e594e8942a54fb43346a4e
singhrp <singhrp@UWM014851.ad.uwm.edu>
2025-02-27 11:10:26 -0600

    Committing Partial Implementation

---
+++ src/edu/uwm/cs351/ArrayCollection.java
@@ -0,0 +1,84 @@
+package edu.uwm.cs351;
+
+import java.util.AbstractCollection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+public class ArrayCollection<E> extends AbstractCollection<E> {
+     private final int INITIAL_CAPACITY = 1; 
+	 @SuppressWarnings("unchecked")
+	private E[] makeArray(int cap) {
+		return (E[]) new Object[cap];
+	}
+
+	E[] data;
+	int manyItems;
+	int version;
+	 /**
+	  * Return the element at given index
+	  * @param index
+	  * @return
+	  */
+	 // Implementation
+	 public E getData(int index) {
+		 return data[index];
+	}
+	@Override //Implementation
+	public boolean add(E e) {
+		ensureCapacity(manyItems+1);
+		data[manyItems++] = e;
+		version++;
+		return true;
+	}
+	
+	@Override //Required
+	public int size() {
+		return manyItems;
+	}
+	private void ensureCapacity(int cap) {
+		if(data.length > cap) return;
+        int newSize = cap; 
+		if(data.length*2 < cap) newSize = data.length*2;
+		E[] newData = makeArray(newSize);
+		for(int i=0;i<manyItems;i++) {
+			newData[i]= data[i];
+		}
+	}
+	public ArrayCollection() {
+		 data = makeArray(INITIAL_CAPACITY);
+		 manyItems = 0;
+		 version =0;
+	 }
+	@Override //Required
+	public Iterator<E> iterator() {
+		return new myIterator();
+	}
+	
+	private class myIterator implements Iterator<E>{
+        int currentIndex;
+        boolean canRemove;
+        int colVersion;
+        
+        private void checkVersion() {
+        	if(colVersion!=version) throw new ConcurrentModificationException("Stale State");
+        }
+        myIterator(){
+        	currentIndex  = -1;
+        	canRemove = false;
+        	colVersion = version;
+        }
+		@Override //Required
+		public boolean hasNext() {
+			checkVersion();
+			return (canRemove?currentIndex+1:currentIndex)<manyItems;
+		}
+
+		@Override //Required
+		public E next() {
+			checkVersion();
+			return data[++currentIndex];
+		}
+		
+	}
+
+}
