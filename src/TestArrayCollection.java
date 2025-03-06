2f687b3cfe957de6363df2afa5ce4f9b7e4ee97e
singhrp <singhrp@UWM014851.ad.uwm.edu>
2025-03-06 10:25:08 -0600

    fix error in test.

--- src/TestArrayCollection.java
+++ src/TestArrayCollection.java
@@ -10,7 +10,7 @@ public class TestArrayCollection extends TestCollection<String> {
 	public void test90() {
 		ArrayCollection<Integer> ac = new ArrayCollection<>();
 		ac.add(Integer.valueOf(42));
-		Integer[] result = ac.getData();
-		System.out.println("a[0] = " + result[0]);
+		/*Integer[] result = ac.getData();
+		System.out.println("a[0] = " + result[0]);*/
 	}
 }
