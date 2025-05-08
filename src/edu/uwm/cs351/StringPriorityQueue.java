116361979bca710c113f191b56f075c70d196503
John T Boyland <boyland@uwm.edu>
2025-05-08 11:24:30 -0500

    AFter lecture 5/8

--- src/edu/uwm/cs351/StringPriorityQueue.java
+++ src/edu/uwm/cs351/StringPriorityQueue.java
@@ -1,6 +1,26 @@
 package edu.uwm.cs351;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class StringPriorityQueue {
+	List<String> heap = new ArrayList<>();
+	
+	private void siftUp(int from) {
+		String value = heap.get(from);
+		while (from != 0) {
+			int front = (from - 1) / 2;
+			String other = heap.get(front);
+			if (other.compareTo(value) > 0) {
+				heap.set(front, value);
+				heap.set(from, other);
+				from = front;
+			} else {
+				break;
+			}
+		}
+	}
 	
 	/**
 	 * Add a string to the pq
@@ -8,7 +28,9 @@ public class StringPriorityQueue {
 	 * @return true
 	 */
 	public boolean add(String x) {
-		return false; // TODO
+		heap.add(x);
+		siftUp(heap.size()-1);
+		return true;
 	}
 
 	/**
@@ -16,7 +38,12 @@ public class StringPriorityQueue {
 	 * @return first string
 	 */
 	public String remove() {
-		return "";
+		if (isEmpty()) throw new NoSuchElementException("no more");
+		String result = heap.get(0);
+		String last = heap.remove(heap.size()-1);
+		heap.set(0, last);
+		// TODO: siftDown
+		return result;
 	}
 	
 	/**
@@ -24,6 +51,6 @@ public class StringPriorityQueue {
 	 * @return whether it is empty.
 	 */
 	public boolean isEmpty() {
-		return false;
+		return heap.isEmpty();
 	}
 }
